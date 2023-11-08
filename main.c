602f6056c25a83549be49a812ba0e95972ffea21
Zach Ramsey <zramsey11@gmail.com>
2023-11-08 11:36:02 -0600

    A bit of cleanup

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 #define DAC_ADDR  0x58        // DAC address
 #define MYUBRR    FOSC/16/BAUD-1
 
-//function declarations:
+/* ============== Function Declarations ============== */
 void getVoltage();
 void getMultiVoltage();
 void setVoltage();
@@ -20,100 +20,93 @@ void USART_Init(unsigned int);
 unsigned char USART_Receive( void );
 void USART_Transmit( unsigned char );
 
-//Variable declarations
-int addr = 0x58; // DAC address
-char stringEndMArker = '\n';   //indicates the end of the serial in msg
-char charIn;                  //individual character
-char charInArray[10];         //array of characters
-char command;                 //stores the command from string in
+/* ============== Variable Declarations ============== */
+int addr = 0x58;        // DAC address
+char charIn;            // individual character
+char charInArray[10];   // array of characters
+char command;           // stores the command from string in
+char p1[2];             // char array for first argument
+char p2[4];             // char array for 2nd argument
 
-//parse variables
-char p1[2];                   //char array for first argument
-char p2[4];                   //char array for 2nd argument
 
+/* ================ Setup & Main Loop ================ */
+
+/* Initialization */
 void setup() {
-  //init i2c
-  tw_init(TW_FREQ_400K, true); // set I2C Frequency, enable internal pull-up
-
-  //init serial communication
-  //set baud rate
-  USART_Init(MYUBRR);
+  tw_init(TW_FREQ_400K, true);  // Init I2C; set freq, enable internal pull-up
+  
+  // Init serial communication
+  UBRR0H = (unsigned char)(MYUBRR>>8);  // Set Baud rate
+  UBRR0L = (unsigned char)MYUBRR;
+  UCSR0B = (1<<RXEN0)|(1<<TXEN0);       // Enable reciever and transmitter
+  UCSR0C = (1<<USBS0)|(3<<UCSZ00);      // Set frame format: 8data, 2stop bit
 }
+
+/* Main Loop */
+void loop() {
+  delay(100);
+}
+
+/* ============ Interupt Service Routines ============ */
 
 //serial recieve interupt
 ISR(USART_RX_vect){
-  //check characters and or strings and perform necissary actions
+  // Read serial input into charInArray
   for (int i = 0; i < 10; i++) {
-    charIn = USART_Receive; //read string from serial, note, timeout is 1sec
-    charInArray[i] = charIn; //save each character
-    if(charIn == stringEndMArker){
-      i = 10;               //if we hit the end marker, stop filling the array
-    }
+    charIn = USART_Receive;       // read string from serial; 1s timeout
+    charInArray[i] = charIn;      // save each character
+    if(charIn == '\n') break;     // stop reading if newline char detected
   }
-  //Pick out commands and variables.
-  command = charInArray[0];       //the command is stored in position zero on the array
+  // Handle different commands based on first character of input
+  command = charInArray[0];
   if(command == 'G'){ getVoltage(); }
   else if(command == 'M'){ getMultiVoltage(); }
   else if(command == 'S'){ setVoltage(); }
 }
 
-void loop() {
-  delay(100);
+/* ========== Serial Communication Routines ========== */
+
+//Serial recieve routine
+unsigned char USART_Receive( void ) {
+while ( !(UCSR0A & (1<<RXC0)) ) ;     // Wait for data to be received
+return UDR0;                          // Get and return received data from buffer
 }
 
-//Serial init
-void USART_Init(unsigned int ubrr){
-  /*Set Baud rate*/
-  UBRR0H = (unsigned char)(ubrr>>8);
-  UBRR0L = (unsigned char)ubrr;
-  //Enable reciever and transmitter
-  UCSR0B = (1<<RXEN0)|(1<<TXEN0);
-  /* Set frame format: 8data, 2stop bit */
-  UCSR0C = (1<<USBS0)|(3<<UCSZ00);
-}
-//Serial recieve routine
-unsigned char USART_Receive( void )
-{
-/* Wait for data to be received */
-while ( !(UCSR0A & (1<<RXC0)) )
-;
-/* Get and return received data from buffer */
-return UDR0;
-}
 //serial transmit routine
 void USART_Transmit( unsigned char data ){
-  /* Wait for empty transmit buffer */
-  while ( !( UCSR0A & (1<<UDRE0)) );
-
-/* Put data into buffer, sends the data */
-UDR0 = data;
+  while ( !( UCSR0A & (1<<UDRE0)) );  // Wait for empty transmit buffer
+  UDR0 = data;                        // Put data into buffer, sends the data
 }
 
+/* ============ Command Handling Routines ============ */
+
+/* Parse input command parameters */
 void parseCommand() {
-  //Clear work arrays so the string conversions dont get confused (we set them to ascii 0)
-  memset(p1, '\0', sizeof(p1));
-  memset(p2, '\0', sizeof(p2));
-  
-  //arrayIndex is shared between the two for loops to conserve position in the charInArray. This is to ensure we can read each argument 
+  // Clear previous command arguments for new command
+  memset(p1, 0, sizeof(p1));
+  memset(p2, 0, sizeof(p2));
+
+  // Store first argument; start after first delimiter, stop at next delimiter ','
   int arrayIndex = 0;
   for (arrayIndex = 2; charInArray[arrayIndex] != ','; arrayIndex++) {  //saves the first argument
     p1[arrayIndex] = charInArray[arrayIndex];
   }
-  //we use next index to offset the p2 array so that we dont go out of bounds
-  int nextIndex = arrayIndex;
-  for (arrayIndex++; charInArray[arrayIndex] != '\n'; arrayIndex++) {   //this saved the 2nd argument
+
+  // Store second argument; start after second delimeter, stop at newline '\n'
+  arrayIndex++;                 // increment to skip the delimiter
+  int nextIndex = arrayIndex;   // store start index of second argument
+  for (arrayIndex; charInArray[arrayIndex] != '\n'; arrayIndex++) {
     p2[arrayIndex - nextIndex] = charInArray[arrayIndex];
   }
 }
 
-// Get single voltage from ADC
+/* Get single voltage from ADC */
 void getVoltage(){          //print the voltage at the ADC
   // print("V=")
   // print((analogRead(0) * (5.0/1023.0)))
 }
 
-// Get multiple voltages from ADC
-
+/* Get multiple voltages from ADC */
 void getMultiVoltage(){
   /*
   int n;
@@ -131,7 +124,7 @@ void getMultiVoltage(){
   */
 }
 
-// Set the voltage of a specific DAC channel
+/* Set the voltage of a specific DAC channel */
 void setVoltage(){
   int c = strtol(p1, NULL, 10);
   float v = strtod(p2, NULL);
