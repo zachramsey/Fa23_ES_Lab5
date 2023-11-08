aa57c4f18e0f1e5e0c2b00b6b65af14545cacd55
Zach Ramsey <zramsey11@gmail.com>
2023-11-08 17:16:05 -0600

    Just need to make set work

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include <avr/io.h>
-//#include <util/delay.h>
+#include <util/delay.h>
 #include <avr/interrupt.h>
-#include <twi_master.h>
+#include <i2cmaster.h>
 #include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define FOSC      16000000    // clock speed
 #define BAUD      9600        // baudrate
-#define DAC_ADDR  0x58        // DAC address
+#define DAC_ADDR  0b01011000  // DAC address
 #define MYUBRR    FOSC/16/BAUD-1
 
 /* ============== Function Declarations ============== */
@@ -16,66 +17,44 @@ void getVoltage();
 void getMultiVoltage();
 void setVoltage();
 void parseCommand();
-void USART_Init(unsigned int);
 unsigned char USART_Receive( void );
-void USART_Transmit( unsigned char );
+void USART_Transmit( unsigned char[] );
 
 /* ============== Variable Declarations ============== */
 int addr = 0x58;        // DAC address
-char charIn;            // individual character
 char charInArray[10];   // array of characters
 char command;           // stores the command from string in
 char p1[2];             // char array for first argument
 char p2[4];             // char array for 2nd argument
 
-
-/* ================ Setup & Main Loop ================ */
+/* ====================== Setup ====================== */
 
 /* Initialization */
 void setup() {
-  tw_init(TW_FREQ_400K, true);  // Init I2C; set freq, enable internal pull-up
+  i2c_init();   // Init I2C
   
   // Init serial communication
-  UBRR0H = (unsigned char)(MYUBRR>>8);  // Set Baud rate
-  UBRR0L = (unsigned char)MYUBRR;
+  unsigned int ubrr = MYUBRR;
+  UBRR0H = (unsigned char) (ubrr>>8);  // Set Baud rate
+  UBRR0L = (unsigned char) ubrr;
   UCSR0B = (1<<RXEN0)|(1<<TXEN0);       // Enable reciever and transmitter
   UCSR0C = (1<<USBS0)|(3<<UCSZ00);      // Set frame format: 8data, 2stop bit
-}
-
-/* Main Loop */
-void loop() {
-  delay(100);
-}
-
-/* ============ Interupt Service Routines ============ */
-
-//serial recieve interupt
-ISR(USART_RX_vect){
-  // Read serial input into charInArray
-  for (int i = 0; i < 10; i++) {
-    charIn = USART_Receive;       // read string from serial; 1s timeout
-    charInArray[i] = charIn;      // save each character
-    if(charIn == '\n') break;     // stop reading if newline char detected
-  }
-  // Handle different commands based on first character of input
-  command = charInArray[0];
-  if(command == 'G'){ getVoltage(); }
-  else if(command == 'M'){ getMultiVoltage(); }
-  else if(command == 'S'){ setVoltage(); }
 }
 
 /* ========== Serial Communication Routines ========== */
 
 //Serial recieve routine
 unsigned char USART_Receive( void ) {
-while ( !(UCSR0A & (1<<RXC0)) ) ;     // Wait for data to be received
-return UDR0;                          // Get and return received data from buffer
+  while ( !(UCSR0A & (1<<RXC0)) ) ;     // Wait for data to be received
+  return UDR0;                          // Get and return received data from buffer
 }
 
 //serial transmit routine
-void USART_Transmit( unsigned char data ){
-  while ( !( UCSR0A & (1<<UDRE0)) );  // Wait for empty transmit buffer
-  UDR0 = data;                        // Put data into buffer, sends the data
+void USART_Transmit( unsigned char data[] ){
+  for (int i = 0; i < strlen(data); i++) {
+    while ( !( UCSR0A & (1<<UDRE0)) );  // Wait for empty transmit buffer
+    UDR0 = data[i];                        // Put data into buffer, sends the data
+  }
 }
 
 /* ============ Command Handling Routines ============ */
@@ -87,47 +66,62 @@ void parseCommand() {
   memset(p2, 0, sizeof(p2));
 
   // Store first argument; start after first delimiter, stop at next delimiter ','
-  int arrayIndex = 0;
-  for (arrayIndex = 2; charInArray[arrayIndex] != ','; arrayIndex++) {  //saves the first argument
-    p1[arrayIndex] = charInArray[arrayIndex];
+  int arrayIndex; //Note, we initialize arrayIndex outside of the scope because its value is shared between the two foor loops
+  for (arrayIndex = 2; charInArray[arrayIndex] != ','; arrayIndex++) {
+    p1[arrayIndex-2] = charInArray[arrayIndex];
   }
 
   // Store second argument; start after second delimeter, stop at newline '\n'
-  arrayIndex++;                 // increment to skip the delimiter
-  int nextIndex = arrayIndex;   // store start index of second argument
-  for (arrayIndex; charInArray[arrayIndex] != '\n'; arrayIndex++) {
+  int nextIndex = ++arrayIndex;   // store start index of second argument
+  for (; charInArray[arrayIndex] != '\n'; arrayIndex++) {
     p2[arrayIndex - nextIndex] = charInArray[arrayIndex];
   }
 }
 
 /* Get single voltage from ADC */
-void getVoltage(){          //print the voltage at the ADC
-  // print("V=")
-  // print((analogRead(0) * (5.0/1023.0)))
+void getVoltage(){
+  ADMUX = 0b01000000;       // Select ADC0
+  ADCSRA = 0b11000111;      // Enable ADC, start conversion, prescaler = 128
+  while(ADCSRA & (1<<ADSC));  // Wait for conversion to finish
+  float voltage = ((float)ADC * 5.0)/1023.0;  // Convert ADC value to voltage
+  unsigned char string[64];
+  dtostrf(voltage, 4, 2, string);
+  USART_Transmit("V=");
+  USART_Transmit(string);
+  USART_Transmit("\n");
 }
 
 /* Get multiple voltages from ADC */
 void getMultiVoltage(){
-  /*
-  int n;
-  int dt;
   parseCommand();
-  n = atoi(p1);
-  dt = atoi(p2);
-
+  int n = strtol(p1, NULL, 10);
+  int dt = strtol(p2, NULL, 10);
   for (int i = 0; i < n; i++) {
-    
-    Serial.print("V=");
-    Serial.print((analogRead(0) * (5.0/1023.0)));
-    delay(dt*1000);
+    getVoltage();
+    for (int j = 0; j < dt; j++) _delay_ms(1000);
   }
-  */
 }
 
 /* Set the voltage of a specific DAC channel */
 void setVoltage(){
+  USART_Transmit("SetingV\n");
+  i2c_start(DAC_ADDR);  // Start I2C communication
   int c = strtol(p1, NULL, 10);
-  float v = strtod(p2, NULL);
-  uint8_t n = round(v * 256 / 5); // Convert voltage to 8-bit number
-  tw_master_transmit(DAC_ADDR, n, sizeof(n), false);
+  i2c_write(c);         // Send channel number
+  int n = round(strtod(p2, NULL) * 255.0 / 5.0); // Convert voltage to 8-bit number
+  i2c_write(n);         // Send voltage
+  i2c_stop();           // Stop I2C communication
+}
+
+/* Main Loop */
+void loop() {
+  for (int i = 0; i < 10; i++) {
+    charInArray[i] = USART_Receive();      // save each character
+    if(charInArray[i] == '\n') break;     // stop reading if newline char detected
+  }
+  // Handle different commands based on first character of input
+  command = charInArray[0];
+  if(command == 'G'){ getVoltage(); }
+  else if(command == 'M'){ getMultiVoltage(); }
+  else if(command == 'S'){ setVoltage(); }
 }
